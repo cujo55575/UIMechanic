45030ee5fb99721b8c780a4cd8929a6b01c156d9
cujo55575 <44086813+cujo55575@users.noreply.github.com>
2019-06-28 16:04:08 +0630

    Add files via upload

---
+++ Assets/Scripts/UITable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UITable : MonoBehaviour
+{
+    [SerializeField]
+    Button[] buttons;
+    // Use this for initialization
+    void Start()
+    {
+        buttons[0].onClick.AddListener(turnLeftTheUITable);
+        buttons[1].onClick.AddListener(turnRightTheUITable);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void turnLeftTheUITable()
+    {
+        Debug.Log("This is left");
+    }
+    void turnRightTheUITable()
+    {
+        Debug.Log("This is right");
+    }
+
+}
